0b2e96626b0215d7c7398c311d190f659f0b05c9
Franco Vilca <75865914+fra92nk@users.noreply.github.com>
2024-04-29 19:08:52 -0300

    Pruebas

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hola = "hola mother fuker!";
+
+            System.Console.WriteLine(hola);
+            
+            Console.Read();//espera que le ingresemos UnauthorizedAccessException tecla
+        }
+    }
+}
